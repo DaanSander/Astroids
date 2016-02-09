ab281691b49c962579620bcb9dd3c0ee37a703dd
Daan <Daan@eriknoel.nl>
2016-02-09 21:16:43 +0100

    Updated collision

--- src/com/daansander/astroids/entity/Entity.java
+++ src/com/daansander/astroids/entity/Entity.java
@@ -26,11 +26,11 @@ public abstract class Entity {
 			if (!entity.destroyed) {
 				int xMin = this.x;
 				int yMin = this.y;
-				
+
 				int xMax = this.x + collisionSize;
-				int yMax = this.y + collisionSize; 
-				
-				if(entity.x < xMax && entity.x > xMin && entity.y < yMax && entity.y > yMin) {
+				int yMax = this.y + collisionSize;
+
+				if (entity.x < xMax && entity.x > xMin && entity.y < yMax && entity.y > yMin) {
 					if (entity instanceof EntityAstroid && this instanceof EntityAstroid) {
 						((MovingEntity) entity).movementX *= -1;
 						((MovingEntity) entity).movementY *= -1;
@@ -46,47 +46,8 @@ public abstract class Entity {
 						// destroy();
 						// return true;
 						// }
-					} 
-//					else if (!(entity instanceof EntityProjectile)) {
-//						return true;
-//					}
+					}
 				}
-//				for (int y = this.y; y < (this.y + collisionSize); y++) {
-//					for (int x = this.x; x < (this.x + collisionSize); x++) {
-//						
-//						if (x == entity.x + entity.collisionSize / 2 && y == entity.y + entity.collisionSize) {
-//							if (entity instanceof EntityAstroid && this instanceof EntityAstroid) {
-//								((MovingEntity) entity).movementX *= -1;
-//								((MovingEntity) entity).movementY *= -1;
-//								return false;
-//							} else if (entity instanceof EntityPlayer && this instanceof EntityAstroid) {
-//								// if (!((EntityPlayer) entity).invincible) {
-//								Game.game.level.clear();
-//								System.err.println("You have been hit by an astroid!");
-//								Game.game.started = false;
-//								((EntityPlayer) entity).dead = true;
-//								return true;
-//								// } else {
-//								// destroy();
-//								// return true;
-//								// }
-//							} else if (!(entity instanceof EntityProjectile)) {
-//
-//								return true;
-//							}
-//							// else if (entity instanceof EntityShield && this
-//							// instanceof EntityAstroid) {
-//							// this.destroy();
-//							// return true;
-//							// } else if (entity instanceof EntityAstroid &&
-//							// this instanceof EntityShield) {
-//							// entity.destroy();
-//							// return true;
-//							// }
-//
-//						}
-//					}
-//				}
 			}
 		}
 		return false;
